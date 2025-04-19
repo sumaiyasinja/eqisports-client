ad930f9032010251820e291ccd97abee93eddc1e
Sumaiya Sinja <sumiyasinja@gmail.com>
2025-04-19 21:43:46 +0600

    hosting on reload 404 issue solved

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
             <Banner></Banner>
             <EquipmentSection equipments={equipments}/>
             <CategoriesSection></CategoriesSection>
-            <TestimonialSection></TestimonialSection>
             <FAQSection></FAQSection>
+            <TestimonialSection></TestimonialSection>
         </div>
     );
 };
